4122fa713550a45ba6b4cf58183dfc865e5b35be
bpiercy <brandon.piercy@gmail.com>
2016-02-11 17:20:57 -0500

    0.1.10 adding FormKeep integration to contact form

--- js/contact_me.js
+++ js/contact_me.js
@@ -18,7 +18,7 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "././mail/contact_me.php",
+                url: "https://formkeep.com/f/a901ebf67646",
                 type: "POST",
                 data: {
                     name: name,
