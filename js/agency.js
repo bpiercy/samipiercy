447995a1c05489837cffbae375f979b7388a5b2b
bpiercy <brandon.piercy@gmail.com>
2016-02-13 15:39:43 -0500

    0.1.19 fixing title scroll and phone resolution color schemes

--- js/agency.js
+++ js/agency.js
@@ -8,6 +8,7 @@
 $(function() {
     $('.page-scroll').bind('click', function(event) {
         var $anchor = $(this);
+        console.log($($anchor.attr('href')));
         $('html, body').stop().animate({
             scrollTop: $($anchor.attr('href')).offset().top
         }, 1000, 'easeInOutExpo');
